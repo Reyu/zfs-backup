8bfdf4077b6118f2e252aee0be3d826ff834dcb7
Tim Millican <tim.millican@gmail.com>
2018-09-10 18:26:14 -0400

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,48 @@
+"""
+Mangaged ZFS Backup Script
+"""
+from setuptools import find_packages, setup
+
+dependencies = ['click']
+
+setup(
+    name='zfs-backup',
+    version='0.1.0',
+    url='https://github.com/Reyu/zfs-backup',
+    license='BSD',
+    author='Tim Millican',
+    author_email='reyu@reyuzenfold.com',
+    description='Mangaged ZFS Backup Script',
+    long_description=__doc__,
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=dependencies,
+    entry_points={
+        'console_scripts': [
+            'zbackup = zfs_backup.cli:main',
+        ],
+    },
+    classifiers=[
+        # As from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        # 'Development Status :: 1 - Planning',
+        # 'Development Status :: 2 - Pre-Alpha',
+        # 'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
+        # 'Development Status :: 5 - Production/Stable',
+        # 'Development Status :: 6 - Mature',
+        # 'Development Status :: 7 - Inactive',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS',
+        'Operating System :: Unix',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
